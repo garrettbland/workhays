e2b77447b92d1763ee6a29d8193cdd2fb9ac7f12
Lane <lanepatterson32@gmail.com>
2020-02-02 15:56:16 -0600

    Privacy Policy
    Added privacy policy

--- routes/routes.js
+++ routes/routes.js
@@ -18,5 +18,7 @@ var job = require('../controllers/job')
 // routes and route type (get, put, post, delete)
 router.get('/', home.list)
 router.get('/jobs/:jobId', job.get)
+router.get('/privacy', function(req, res) {  res.render('pages/privacy');});
+
 
 module.exports = router
