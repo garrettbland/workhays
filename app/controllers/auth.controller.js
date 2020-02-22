10b7f911acfaf66f96e659b31fc261b4ac146242
garrettbland <gmorganbland@gmail.com>
2020-02-22 13:34:00 -0600

    setup password reset

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -2,6 +2,8 @@ var Models = require('../models')
 var path = require('path')
 var env = process.env.NODE_ENV || 'development'
 var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env]
+const uuidv1 = require('uuid/v1')
+var bCrypt = require('bcrypt-nodejs')
 
 exports.signup = function (req, res) {
   res.render('pages/signup', {
@@ -23,7 +25,6 @@ exports.logout = function (req, res) {
 
 exports.password_reset = async (req, res) => {
   try {
-
     // Make sure user exists in db
     const user = await Models.user.findOne({
       where: {
@@ -31,14 +32,15 @@ exports.password_reset = async (req, res) => {
       }
     })
 
-    if(!user) {
+    if (!user) {
       console.log('=====> USER NOT FOUND')
       throw 'User not found'
     }
 
     // update user with random token and expiration date
-    let random_token = '1233'
-    let expires = new Date(Date.now() + 86400)
+    let random_token = uuidv1()
+    let tomorrow = new Date()
+    let expires = tomorrow.setDate(tomorrow.getDate() + 1)
 
     let updated_fields = {
       password_reset_token: random_token,
@@ -51,33 +53,116 @@ exports.password_reset = async (req, res) => {
       }
     })
 
-    if(!update_user) {
+    if (!update_user) {
       console.log('=====> USER NOT UPDATED')
       throw 'User not updated'
     }
 
-    var api_key = config.mailgun_api_key;
-    var domain = 'mg.workhays.com';
-    var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
-    
+    var api_key = config.mailgun_api_key
+    var domain = 'mg.workhays.com'
+    var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain })
+
     var data = {
       from: 'Work Hays <support@workhays.com>',
       to: req.body.email,
       subject: 'Password Reset',
       text: `Click here to reset your password. http://localhost:3000/password-update?token=${random_token}. This will expire in 24 hours`
-    };
-    
+    }
+
     mailgun.messages().send(data, function (error, body) {
-      console.log(body);
-    });
+      console.log(body)
+    })
 
     res.status(200)
-    res.redirect('/')
-
+    req.flash(
+      'message',
+      'An email has been sent to your account with instructions'
+    )
+    res.redirect('/password-reset')
   } catch (err) {
-        console.log('Error in password_reset =====>')
-        console.log(err)
+    console.log('Error in password_reset =====>')
+    console.log(err)
+    res.status(200)
+    res.render('error')
+  }
+}
+
+exports.password_update = async (req, res) => {
+  try {
+    if (!req.query.token) throw 'Token not found'
+
+    // Make sure user exists in db and token matches
+    const user = await Models.user.findOne({
+      where: {
+        password_reset_token: req.query.token
+      }
+    })
+
+    if (!user) {
+      console.log('=====> USER NOT FOUND')
+      throw 'User not found'
+    }
+
+    const now = new Date()
+    const expires = user.dataValues.password_reset_token_expires
+
+    if (now < expires) {
+      console.log('user can reset password ===>')
       res.status(200)
-      res.render('error')
+      res.render('pages/passwordupdate', {
+        token: req.query.token
+      })
+    } else {
+      console.log('token is expired ===>')
+      throw 'Token expired'
+    }
+  } catch (err) {
+    console.log('Error in password update ===>')
+    res.status(404)
+    res.render('error')
+  }
+}
+
+exports.change_password = async (req, res) => {
+  try {
+    // Make sure user exists in db and token matches
+    const user = await Models.user.findOne({
+      where: {
+        password_reset_token: req.body.token
+      }
+    })
+
+    if (!user) throw 'User not found'
+
+    var generateHash = function (password) {
+      return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null)
+    }
+
+    var userPassword = generateHash(req.body.password)
+
+    let updated_fields = {
+      password: userPassword,
+      password_reset_token: null,
+      password_reset_token_expires: null
+    }
+
+    const updated_user = await Models.user.update(updated_fields, {
+      where: {
+        email: user.dataValues.email
+      }
+    })
+
+    if (!updated_user) throw 'Password not updated'
+
+    if (updated_user) {
+      res.status(200)
+      req.flash('loginMessage', 'Password updated successfully')
+      res.redirect('/signin')
+    }
+  } catch (err) {
+    console.log(err.message)
+    console.log('Error in change password ===>')
+    res.status(404)
+    res.render('error')
   }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -8,87 +8,94 @@ controller to handle logic.
 ===========================================================================
 */
 
-var express = require("express");
-var passport = require("passport");
-var router = express.Router();
-var middleware = require("./middleware");
+var express = require('express')
+var passport = require('passport')
+var router = express.Router()
+var middleware = require('./middleware')
 
 // controllers
-var jobController = require("../app/controllers/job.controller");
-var authController = require("../app/controllers/auth.controller");
-var accountController = require("../app/controllers/account.controller");
-var employerController = require("../app/controllers/employer.controller");
+var jobController = require('../app/controllers/job.controller')
+var authController = require('../app/controllers/auth.controller')
+var accountController = require('../app/controllers/account.controller')
+var employerController = require('../app/controllers/employer.controller')
 
 // === public routes ===
-router.get("/", jobController.list_jobs);
-router.get("/jobs/:jobId", jobController.get_job);
-router.get("/contact", (req, res) => res.render("pages/contact"));
-router.get("/events", (req, res) => res.render("pages/events"));
-router.get("/privacy", (req, res) => res.render("pages/privacy"));
-router.get("/employers", employerController.list_employers);
-router.get("/employers/:employerId", employerController.get_employer);
-router.get("/info", (req, res) => res.render("pages/info"));
-router.get("/help", (req, res) => res.render("pages/help"));
-router.get("/terms", (req, res) => res.render("pages/terms"));
-router.get("/login", (req, res) => res.render("pages/login"));
-router.get("/password-reset", (req, res) => res.render("pages/passwordreset"));
+router.get('/', jobController.list_jobs)
+router.get('/jobs/:jobId', jobController.get_job)
+router.get('/contact', (req, res) => res.render('pages/contact'))
+router.get('/events', (req, res) => res.render('pages/events'))
+router.get('/privacy', (req, res) => res.render('pages/privacy'))
+router.get('/employers', employerController.list_employers)
+router.get('/employers/:employerId', employerController.get_employer)
+router.get('/info', (req, res) => res.render('pages/info'))
+router.get('/help', (req, res) => res.render('pages/help'))
+router.get('/terms', (req, res) => res.render('pages/terms'))
+router.get('/login', (req, res) => res.render('pages/login'))
+router.get('/password-reset', (req, res) => {
+  res.render('pages/passwordreset', {
+    message: req.flash('message')
+  })
+})
 router.post('/password-reset', authController.password_reset)
 
-router.get("/password-update", (req, res) =>
-  res.render("pages/passwordupdate")
-);
-router.get("/signup", authController.signup);
-router.get("/signin", authController.signin);
+router.get('/password-update', authController.password_update)
+router.post('/password-update', authController.change_password)
+router.get('/signup', authController.signup)
+router.get('/signin', authController.signin)
 router.post(
-  "/signup",
-  passport.authenticate("local-signup", {
-    successRedirect: "/account",
-    failureRedirect: "/signup"
+  '/signup',
+  passport.authenticate('local-signup', {
+    successRedirect: '/account',
+    failureRedirect: '/signup'
   })
-);
+)
 router.post(
-  "/signin",
-  passport.authenticate("local-signin", {
-    successRedirect: "/account",
-    failureRedirect: "/signin"
+  '/signin',
+  passport.authenticate('local-signin', {
+    successRedirect: '/account',
+    failureRedirect: '/signin'
   })
-);
-router.get("/logout", authController.logout);
+)
+router.get('/logout', authController.logout)
 
 // === private routes ===
-router.get("/account", middleware.isLoggedIn, accountController.index);
-router.get("/job-listing-archive", middleware.isLoggedIn, accountController.list_archived_jobs);
-router.get("/job-listing-new", middleware.isLoggedIn, function(req, res) {
-  res.render("pages/joblistingnew");
-});
+router.get('/account', middleware.isLoggedIn, accountController.index)
 router.get(
-  "/jobs/edit/:jobId",
+  '/job-listing-archive',
+  middleware.isLoggedIn,
+  accountController.list_archived_jobs
+)
+router.get('/job-listing-new', middleware.isLoggedIn, function (req, res) {
+  res.render('pages/joblistingnew')
+})
+router.get(
+  '/jobs/edit/:jobId',
   middleware.isLoggedIn,
   accountController.edit_job
-);
+)
 
 // auth only routes
 // To do: add Auth lol
-router.post("/jobs", jobController.create_job);
-router.post("/jobs/edit/:jobId", jobController.update_job);
+router.post('/jobs', jobController.create_job)
+router.post('/jobs/edit/:jobId', jobController.update_job)
 // router.delete("/jobs/:jobId", jobController.archive_job);
-router.post("/employers/edit/:employerId", employerController.update_employer);
+router.post('/employers/edit/:employerId', employerController.update_employer)
 
-router.get('/emailtest', function(req, res) {
-  var api_key = '0b6a3b5c0b9b37bc188e42609d3de538-9ce9335e-5d0ed33c';
-  var domain = 'mg.workhays.com';
-  var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
-  
+router.get('/emailtest', function (req, res) {
+  var api_key = '0b6a3b5c0b9b37bc188e42609d3de538-9ce9335e-5d0ed33c'
+  var domain = 'mg.workhays.com'
+  var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain })
+
   var data = {
     from: 'Work Hays <support@workhays.com>',
     to: 'lanepatterson32@gmail.com',
     subject: 'Hello',
     text: 'Testing some Mailgun awesomeness!'
-  };
-  
+  }
+
   mailgun.messages().send(data, function (error, body) {
-    console.log(body);
-  });
+    console.log(body)
+  })
 })
 
-module.exports = router;
+module.exports = router
