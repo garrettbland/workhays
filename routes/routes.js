11b6a128b41aa427e64476c78bc6f9f336c2db0c
garrettbland <gmorganbland@gmail.com>
2020-03-08 10:45:45 -0500

    cleaning up router

--- routes/routes.js
+++ routes/routes.js
@@ -8,23 +8,38 @@ controller to handle logic.
 ===========================================================================
 */
 
+/*
+
+External Scripts
+
+*/
+
 var express = require('express')
 var passport = require('passport')
 var router = express.Router()
 var middleware = require('./middleware')
 
-// controllers
+/*
+
+Controllers
+
+*/
 var jobController = require('../app/controllers/job.controller')
 var authController = require('../app/controllers/auth.controller')
 var accountController = require('../app/controllers/account.controller')
 var employerController = require('../app/controllers/employer.controller')
 var contactController = require('../app/controllers/contact.controller')
 
-// === public routes ===
+/*
+
+Public Routes
+
+*/
 router.get('/', jobController.list_jobs)
 router.get('/jobs/:jobId', jobController.get_job)
-router.get('/contact', (req, res) => res.render('pages/contact'))
-router.post('/contact', contactController.contact_form)
+router.route('/contact')
+  .get((req, res) => res.render('pages/contact'))
+  .post(contactController.contact_form)
 router.get('/events', (req, res) => res.render('pages/events'))
 router.get('/privacy', (req, res) => res.render('pages/privacy'))
 router.get('/employers', employerController.list_employers)
@@ -33,34 +48,36 @@ router.get('/info', (req, res) => res.render('pages/info'))
 router.get('/help', (req, res) => res.render('pages/help'))
 router.get('/terms', (req, res) => res.render('pages/terms'))
 router.get('/login', (req, res) => res.render('pages/login'))
-router.get('/password-reset', (req, res) => {
-  res.render('pages/passwordreset', {
-    message: req.flash('message')
-  })
-})
-router.post('/password-reset', authController.password_reset)
-
-router.get('/password-update', authController.password_update)
-router.post('/password-update', authController.change_password)
-router.get('/signup', authController.signup)
-router.get('/signin', authController.signin)
-router.post(
-  '/signup',
-  passport.authenticate('local-signup', {
-    successRedirect: '/account',
-    failureRedirect: '/signup'
-  })
-)
-router.post(
-  '/signin',
-  passport.authenticate('local-signin', {
+router.route('/signin')
+  .get(authController.signin)
+  .post(passport.authenticate('local-signin', {
     successRedirect: '/account',
     failureRedirect: '/signin'
+  }))
+router.route('/signup')
+  .get(authController.signup)
+  .post(passport.authenticate('local-signup', {
+    successRedirect: '/account',
+    failureRedirect: '/signup'
+  }))
+router.route('/password-reset')
+  .get((req, res) => {
+    res.render('pages/passwordreset', {
+      message: req.flash('message')
+    })
   })
-)
+  .post(authController.password_reset)
+router.route('/password-update')
+  .get(authController.password_update)
+  .post(authController.change_password)
 router.get('/logout', authController.logout)
 
-// === private routes ===
+
+/*
+
+Private Routes
+
+*/
 router.get('/account', middleware.isLoggedIn, accountController.index)
 router.get(
   '/job-listing-archive',
