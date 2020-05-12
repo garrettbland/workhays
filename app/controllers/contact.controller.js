01a612943284c89d8ce7b01c03dc76a7b451d155
garrettbland <gmorganbland@gmail.com>
2020-05-11 20:07:40 -0500

    updated contact form reply-to

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -5,7 +5,6 @@ var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env]
 
 exports.contact_form = async (req, res) => {
     try {
-
         // honey pot - check if hidden honey pot checkbox equals something other than 0
         if (req.body.contact_me_by_phone_only) {
             // the hidden field was checked, treat as spambot
@@ -14,14 +13,19 @@ exports.contact_form = async (req, res) => {
             throw 'Something went wrong'
         }
 
-        if (!req.body.first || !req.body.last || !req.body.email || !req.body.message) {
+        if (
+            !req.body.first ||
+            !req.body.last ||
+            !req.body.email ||
+            !req.body.message
+        ) {
             throw 'All fields must be completed'
         }
 
         // validate email function
-        const validateEmail = (email) => {
-            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(email);
+        const validateEmail = email => {
+            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            return re.test(email)
         }
 
         // checks if email is valid
@@ -35,15 +39,15 @@ exports.contact_form = async (req, res) => {
             message: req.body.message,
         })
 
-        if(!newContact) throw 'Error when creating new contact'
+        if (!newContact) throw 'Error when creating new contact'
 
         var api_key = config.mailgun_api_key
         var domain = 'mg.workhays.com'
         var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain })
 
         var data = {
-            from: 'Work Hays <support@workhays.com>',
-            to: 'workhays@gmail.com,gmorganbland@gmail.com',
+            from: 'Work Hays <no-reply@workhays.com>',
+            to: 'workhays@gmail.com',
             subject: 'Contact Submission',
             html: `<h1>Contact Details</h1><p>A new contact us submission has been submitted from Work Hays. View details below. A copy of this submission was saved.</p><p>
                 <ul>
@@ -55,18 +59,19 @@ exports.contact_form = async (req, res) => {
                         <p>${req.body.message}</p>
                     </li>
                 </ul>
-            </p>`
-          }
-          
+            </p>`,
+        }
+
+        // set reply-to
+        data['h:Reply-To'] = req.body.email
+
         // send the email
-          mailgun.messages().send(data, function (error, body) {
+        mailgun.messages().send(data, function(error, body) {
             console.log(body)
-          })
-        
+        })
+
         // res.sendStatus(200)
         res.redirect('/contact?success=true')
-        
-        
     } catch (err) {
         console.log(err)
         //res.sendStatus(200)
