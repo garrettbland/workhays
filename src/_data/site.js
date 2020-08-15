6a4d7df1bcaf3ed7019f9b9700c24412680cb898
garrettbland <gmorganbland@gmail.com>
2020-08-14 23:58:53 -0500

    Setting up functions, netlify

---
+++ src/_data/site.js
@@ -0,0 +1,14 @@
+module.exports = function () {
+    /**
+     * Global site config
+     * Access values with "site.[key]" globally
+     */
+    return {
+        title: 'Work Hays',
+        description: 'Local Jobs in Hays KS',
+        phone: '',
+        email: '',
+        address: '',
+        facebook: '',
+    }
+}
