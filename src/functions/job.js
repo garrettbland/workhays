634ee0b9386e1d0a8ffcfbdadb15df717a6544e4
garrettbland <gmorganbland@gmail.com>
2020-08-15 18:23:48 -0500

    working on liquid engine

--- src/functions/job.js
+++ src/functions/job.js
@@ -1,8 +1,26 @@
+var path = require('path')
+var { Liquid } = require('liquidjs')
+var engine = new Liquid({
+    root: path.resolve(__dirname, '../_includes'),
+    extname: '.liquid',
+})
+
+// const layout = require('../_layouts/default.liquid')
+// const head = require('../_includes/head.liquid')
+
 exports.handler = async (event, context, callback) => {
     try {
+        // engine
+        //     .parseAndRender('{{name | capitalize}}', { name: 'lucy' })
+        //     .then(console.log)
+
+        var body = await engine.renderFile('layout', {
+            content: '<h1>Test</h1>',
+        })
+
         return {
             statusCode: 200,
-            body: `Hello from a serverless function!`,
+            body: body,
         }
     } catch (err) {
         return { statusCode: 500, body: err.toString() }
