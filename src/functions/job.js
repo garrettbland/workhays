2aca90ed2592828c26c3bad77f7e24ee9cc78036
garrettbland <gmorganbland@gmail.com>
2020-08-17 14:03:06 -0500

    testing out template literals

--- src/functions/job.js
+++ src/functions/job.js
@@ -1,21 +1,22 @@
 var path = require('path')
-var { Liquid } = require('liquidjs')
-var engine = new Liquid({
-    root: path.join(__dirname, '/_includes'),
-})
+// var { Liquid } = require('liquidjs')
+// var engine = new Liquid({
+//     root: path.join(__dirname, '/_includes'),
+// })
+var layout = require('./_includes/head.11ty.js')
 
 exports.handler = async (event, context, callback) => {
     try {
         // get the job id from request
         const jobId = event.queryStringParameters.id
 
-        var body = await engine.renderFile('layout.liquid', {
-            content: `<h1>Job id: ${jobId}</h1>`,
-        })
+        // var body = await engine.renderFile('layout.liquid', {
+        //     content: `<h1>Job id: ${jobId}</h1>`,
+        // })
 
         return {
             statusCode: 200,
-            body: body,
+            body: layout({ jobId: jobId }),
         }
     } catch (err) {
         return { statusCode: 500, body: err.toString() }
