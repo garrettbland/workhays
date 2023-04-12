3a0b3c3b3a489f5eefce602cb374fca20a13d5fc
Garrett Bland <gmorganbland@gmail.com>
2023-04-11 23:18:25 -0500

    fix: code coverage ci job

--- vitest.config.js
+++ vitest.config.js
@@ -6,5 +6,13 @@ export default defineConfig({
     plugins: [react()],
     test: {
         environment: 'jsdom',
+        coverage: {
+            provider: 'istanbul',
+            reporter: ['text', 'json', 'html'],
+            lines: 90,
+            functions: 90,
+            branches: 90,
+            statements: 90,
+        },
     },
 })
